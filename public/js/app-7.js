eae01798c51edfb589c32bfa1b7c127dbc08a3a7
Dee Race <deerace12@gmail.com>
2018-09-21 14:06:35 -0400

    responsive up arrows, app tutorial finished

--- public/js/app-7.js
+++ public/js/app-7.js
@@ -1,11 +1,11 @@
-
 class ProductList extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      products: [],
+      products: []
     };
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
   }
 
   componentDidMount() {
@@ -13,16 +13,25 @@ class ProductList extends React.Component {
   }
 
   handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted.');
+    const nextProducts = this.state.products.map(product => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts
+    });
   }
 
   render() {
-    const products = this.state.products.sort((a, b) => (
-      b.votes - a.votes
-    ));
-    const productComponents = products.map((product) => (
+    const products = this.state.products.sort((a, b) => b.votes - a.votes);
+    const productComponents = products.map(product => (
       <Product
-        key={'product-' + product.id}
+        key={"product-" + product.id}
         id={product.id}
         title={product.title}
         description={product.description}
@@ -33,11 +42,7 @@ class ProductList extends React.Component {
         onVote={this.handleProductUpVote}
       />
     ));
-    return (
-      <div className='ui unstackable items'>
-        {productComponents}
-      </div>
-    );
+    return <div className="ui unstackable items">{productComponents}</div>;
   }
 }
 
@@ -54,29 +59,25 @@ class Product extends React.Component {
 
   render() {
     return (
-      <div className='item'>
-        <div className='image'>
+      <div className="item">
+        <div className="image">
           <img src={this.props.productImageUrl} />
         </div>
-        <div className='middle aligned content'>
-          <div className='header'>
+        <div className="middle aligned content">
+          <div className="header">
             <a onClick={this.handleUpVote}>
-              <i className='large caret up icon' />
+              <i className="large caret up icon" />
             </a>
             {this.props.votes}
           </div>
-          <div className='description'>
-            <a href={this.props.url}>
-              {this.props.title}
-            </a>
-            <p>
-              {this.props.description}
-            </p>
+          <div className="description">
+            <a href={this.props.url}>{this.props.title}</a>
+            <p>{this.props.description}</p>
           </div>
-          <div className='extra'>
+          <div className="extra">
             <span>Submitted by:</span>
             <img
-              className='ui avatar image'
+              className="ui avatar image"
               src={this.props.submitterAvatarUrl}
             />
           </div>
@@ -86,7 +87,4 @@ class Product extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <ProductList />,
-  document.getElementById('content')
-);
+ReactDOM.render(<ProductList />, document.getElementById("content"));
